709422424246c22e22c7a259470881f77bba0d93
XM666-Dev <x94732568960@163.com>
2026-08-02 00:31:20 +0800

    Fix

--- src/main/java/com/xm666/dummycritfix/mixin/dummycritfix/CritRecordMixin.java
+++ src/main/java/com/xm666/dummycritfix/mixin/dummycritfix/CritRecordMixin.java
@@ -1,27 +1,29 @@
 package com.xm666.dummycritfix.mixin.dummycritfix;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import com.xm666.dummycritfix.handler.CritInfoHandler;
+import net.mehvahdjukaar.dummmmmmy.common.CritRecord;
 import net.mehvahdjukaar.dummmmmmy.common.ModEvents;
 import net.mehvahdjukaar.dummmmmmy.common.TargetDummyEntity;
+import net.minecraft.world.damagesource.DamageSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 public class CritRecordMixin {
     @Mixin(value = ModEvents.class, remap = false)
     private static class ModEventsMixin {
-        @ModifyArgs(method = "onEntityDamage", at = @At(value = "INVOKE", target = "Lnet/mehvahdjukaar/dummmmmmy/network/ClientBoundDamageNumberMessage;<init>(Lnet/minecraft/world/entity/LivingEntity;FLnet/minecraft/world/damagesource/DamageSource;Lnet/mehvahdjukaar/dummmmmmy/common/CritRecord;)V"))
-        private static void modifyCritRecord(Args args) {
-            args.set(3, CritInfoHandler.toRecord(args.get(2), args.get(3)));
+        @ModifyArg(method = "onEntityDamage", at = @At(value = "INVOKE", target = "Lnet/mehvahdjukaar/dummmmmmy/network/ClientBoundDamageNumberMessage;<init>(Lnet/minecraft/world/entity/LivingEntity;FLnet/minecraft/world/damagesource/DamageSource;Lnet/mehvahdjukaar/dummmmmmy/common/CritRecord;)V"))
+        private static CritRecord modifyCritRecord(CritRecord record, @Local(argsOnly = true) DamageSource source) {
+            return CritInfoHandler.toRecord(source, record);
         }
     }
 
     @Mixin(value = TargetDummyEntity.class, remap = false)
     private static class TargetDummyEntityMixin {
-        @ModifyArgs(method = "showDamageAndAnimationsToClients", at = @At(value = "INVOKE", target = "Lnet/mehvahdjukaar/dummmmmmy/network/ClientBoundDamageNumberMessage;<init>(Lnet/minecraft/world/entity/LivingEntity;FLnet/minecraft/world/damagesource/DamageSource;Lnet/mehvahdjukaar/dummmmmmy/common/CritRecord;)V"))
-        private void modifyCritRecord(Args args) {
-            args.set(3, CritInfoHandler.toRecord(args.get(2), args.get(3)));
+        @ModifyArg(method = "showDamageAndAnimationsToClients", at = @At(value = "INVOKE", target = "Lnet/mehvahdjukaar/dummmmmmy/network/ClientBoundDamageNumberMessage;<init>(Lnet/minecraft/world/entity/LivingEntity;FLnet/minecraft/world/damagesource/DamageSource;Lnet/mehvahdjukaar/dummmmmmy/common/CritRecord;)V"))
+        private CritRecord modifyCritRecord(CritRecord record, @Local(argsOnly = true) DamageSource source) {
+            return CritInfoHandler.toRecord(source, record);
         }
     }
 }

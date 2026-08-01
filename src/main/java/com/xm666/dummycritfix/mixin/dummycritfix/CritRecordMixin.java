70aac6d086e20a8fbbb06814429179bb52f6ec19
XM666-Dev <x94732568960@163.com>
2026-08-02 00:20:54 +0800

    Fix

--- src/main/java/com/xm666/dummycritfix/mixin/dummycritfix/CritRecordMixin.java
+++ src/main/java/com/xm666/dummycritfix/mixin/dummycritfix/CritRecordMixin.java
@@ -20,7 +20,7 @@ public class CritRecordMixin {
     @Mixin(value = TargetDummyEntity.class, remap = false)
     private static class TargetDummyEntityMixin {
         @ModifyArgs(method = "showDamageAndAnimationsToClients", at = @At(value = "INVOKE", target = "Lnet/mehvahdjukaar/dummmmmmy/network/ClientBoundDamageNumberMessage;<init>(Lnet/minecraft/world/entity/LivingEntity;FLnet/minecraft/world/damagesource/DamageSource;Lnet/mehvahdjukaar/dummmmmmy/common/CritRecord;)V"))
-        private static void modifyCritRecord(Args args) {
+        private void modifyCritRecord(Args args) {
             args.set(3, CritInfoHandler.toRecord(args.get(2), args.get(3)));
         }
     }
